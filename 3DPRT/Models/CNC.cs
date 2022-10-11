a39b811bc0f5f2c24ef173830dd3f87af3316087
Mari Maisuradze <mari.maisura@gmail.com>
2022-10-11 16:07:51 +0300

    Add CNC service class

---
+++ 3DPRT/Models/CNC.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _3DPRT.Models
+{
+    public class CNC:Service
+    {
+        [Required]
+        public BluePrint Image { get; set; }
+        [Required]
+        public string Material { get; set; }
+        [Required]
+        public int MaterialThickness { get; set; }
+        [Required]
+        public int CutDepth { get; set; }
+    }
+}
