eb873497d2f37a64828ab572d8c4acca00c15ae0
Mari Maisuradze <mari.maisura@gmail.com>
2022-10-11 10:11:14 +0300

    Add project files.

---
+++ 3DPRT.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _3DPRT.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
