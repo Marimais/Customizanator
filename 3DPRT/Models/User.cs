caea1ec4b689aacd869c5746bdd35747f30c423d
Mari Maisuradze <mari.maisura@gmail.com>
2022-10-11 15:28:49 +0300

    Add User class to models

---
+++ 3DPRT/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace _3DPRT.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LatsName { get; set; }
+        [Required]
+        public string Email { get; set; }        
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
+        [RegularExpression(@"^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)).+$", ErrorMessage="Must contain at least one digit, one lower and one upper case character.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get ; set; }
+
+        public virtual ICollection<Model> Models { get; set; }
+
+    }
+}
