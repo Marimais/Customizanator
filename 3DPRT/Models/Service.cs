f73f4cd2c8ee1e623c8b7535a069842f69c8bf26
Mari Maisuradze <mari.maisura@gmail.com>
2022-10-11 15:28:34 +0300

    Add services class to models

---
+++ 3DPRT/Models/Service.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _3DPRT.Models
+{
+    public class Service
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string ServiceName { get; set; }
+    }
+}
