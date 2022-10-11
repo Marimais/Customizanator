47619c6d9d376bae738954f5a4d47eeed6e33b87
Mari Maisuradze <mari.maisura@gmail.com>
2022-10-11 16:05:26 +0300

    Add BluePrint class

---
+++ 3DPRT/Models/BluePrint.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace _3DPRT.Models
+{
+    public class BluePrint
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string ImageTitle { get; set; }
+        public string ImagelURL { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
