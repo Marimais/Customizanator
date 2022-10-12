94acfbb220801d7edde504ce8f376b6ef7734e2c
Mari Maisuradze <mari.maisura@gmail.com>
2022-10-12 07:33:37 +0300

    Add Stock class

---
+++ 3DPRT/Models/Stock.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _3DPRT.Models
+{
+    public class Stock
+    {
+        [Required]
+        public Color Color { get; set; }
+        [Required]
+        public PrintingMaterial Material { get; set; }
+    }
+}
