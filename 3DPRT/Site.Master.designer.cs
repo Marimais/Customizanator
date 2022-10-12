e8480449455bd0632c20d21150e22cc35a774b9e
Mari Maisuradze <mari.maisura@gmail.com>
2022-10-12 23:41:03 +0300

    Update master page with link to Sign Up page

--- 3DPRT/Site.Master.designer.cs
+++ 3DPRT/Site.Master.designer.cs
@@ -15,24 +15,6 @@ namespace _3DPRT
     {
 
         /// <summary>
-        /// title control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder title;
-
-        /// <summary>
-        /// lblTitle control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblTitle;
-
-        /// <summary>
         /// mainForm control.
         /// </summary>
         /// <remarks>
@@ -49,15 +31,6 @@ namespace _3DPRT
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder mainContent;
-
-        /// <summary>
-        /// footer control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder footer;
 
         /// <summary>
         /// tblFooter control.
