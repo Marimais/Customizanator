763b26b394013c30939cc5d64cf234c3a87f7983
Mari Maisuradze <mari.maisura@gmail.com>
2022-10-13 14:07:13 +0300

    Add Database context

---
+++ 3DPRT/DataAccess/PrintingDBContext.cs
@@ -0,0 +1,21 @@
+﻿using _3DPRT.Models;
+using System.Data.Entity;
+
+namespace _3DPRT.DataAccess
+{
+    public class PrintingDBContext : DbContext
+    {
+        public PrintingDBContext() : base("PrintingDatabase")
+        {
+
+        }
+
+        public DbSet<User> Users {get;set;}
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<BluePrint> BluePrintrs { get; set; }
+        public DbSet<CNC> Carvings { get; set; }
+        public DbSet<Laser> Engravings { get; set; }
+        public DbSet<Printing> Printings { get; set; }
+    }
+}
