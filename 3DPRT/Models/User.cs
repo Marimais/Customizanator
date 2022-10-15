0e67d3411aa57b5970b5b5256831c44bc65e902b
Mari Maisuradze <mari.maisura@gmail.com>
2022-10-15 16:03:50 +0300

    Remove data validations using anotations from User entity

--- 3DPRT/Models/User.cs
+++ 3DPRT/Models/User.cs
@@ -22,12 +22,10 @@ namespace _3DPRT.Models
         [MaxLength(50)]
         public string Email { get; set; }        
         [Required]
-        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
-        [RegularExpression(@"^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)).+$", ErrorMessage="Must contain at least one digit, one lower and one upper case character.")]
+        [MaxLength(100)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
         public string Password { get ; set; }
-        public Role Role { get; set; }
+        public virtual Role Role { get; set; }
 
         public virtual ICollection<Order> Orders { get; set; }
 
