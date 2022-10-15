2f2adfd991fb5dab27b23c6b6296e1df7734c358
Mari Maisuradze <mari.maisura@gmail.com>
2022-10-15 19:43:39 +0300

    Add arguments to User class constructor

--- 3DPRT/Models/User.cs
+++ 3DPRT/Models/User.cs
@@ -1,23 +1,27 @@
-﻿using System.Collections.Generic;
+﻿#nullable enable
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Web.UI.WebControls;
 
 namespace _3DPRT.Models
 {
     public class User
     {
-        public User(string email, string password)
+        public User(string email, string password, string? firstName, string? lastName, Role role, List<Order>? orders)
         {
             Email = email;
             Password = password;
+            FirstName = firstName;
+            LastName=lastName;
+            Role = role;
+            Orders = orders;
         }
 
         [Key]
         public int ID { get; set; }
         [MaxLength(50)]
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }=string.Empty;
         [MaxLength(50)]
-        public string LatsName { get; set; }
+        public string? LastName { get; set; } = string.Empty;
         [Required]
         [MaxLength(50)]
         public string Email { get; set; }        
@@ -25,9 +29,10 @@ namespace _3DPRT.Models
         [MaxLength(100)]
         [DataType(DataType.Password)]
         public string Password { get ; set; }
+        [Required]
         public virtual Role Role { get; set; }
 
-        public virtual ICollection<Order> Orders { get; set; }
+        public virtual ICollection<Order>? Orders { get; set; }=new List<Order>();
 
     }
 }
