8348170e8736b5735ea322c8bba1deb6d8360543
Mari <mari.maisura@gmail.com>
2022-10-21 12:10:02 +0300

    Add Web API to the project

---
+++ 3DPRT.API/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using _3DPRT.API.Services;
+using _3DPRT.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _3DPRT.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController:ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<User>> GetAll() => UserService.GetAll();
+        
+    }
+}
---
+++ 3DPRT.API/Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿using _3DPRT.DataAccess;
+using _3DPRT.Models;
+
+namespace _3DPRT.API.Services
+{
+    public static class UserService
+    {
+        readonly static PrintingDBContext dbContext =new ();
+        public static List<User> GetAll()
+        {
+            return dbContext.Users.ToList();
+        }
+    }
+}
