27f5ee703f8e1feda672757b1b76e87a199851e1
Mari <mari.maisura@gmail.com>
2022-10-24 12:30:09 +0300

    Add theme switching

---
+++ Customizanator.App/Pages/app.js
@@ -0,0 +1,17 @@
+﻿
+
+const switcher = document.querySelector('#theme-btn');
+
+switcher.addEventListener('click', function () {
+    document.getElementById("mainBody").classList.toggle('light-theme');
+    document.getElementById("mainBody").classList.toggle('dark-theme');
+
+    const className = document.getElementById("mainBody").className;
+    if (className == "light-theme")
+    {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+    console.info(className);
+});
